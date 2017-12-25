f3b48a82595eecd2114c0b1c6a26f923401f10df
cyouk2 <q174221679@gmail.com>
2017-12-25 13:39:27 +0900

    Update NPCHandler.cpp

--- src/game/Entities/NPCHandler.cpp
+++ src/game/Entities/NPCHandler.cpp
@@ -378,7 +378,19 @@ void WorldSession::HandleGossipSelectOptionOpcode(WorldPacket& recv_data)
 
         if (!sScriptDevAIMgr.OnGossipSelect(_player, pGo, sender, action, code.empty() ? nullptr : code.c_str()))
             _player->OnGossipSelect(pGo, gossipListId);
-    }
+    }else if (guid.IsItem())
+	{
+		Item* pItem = GetPlayer()->GetItemByGuid(guid);
+		if (!pItem)
+		{
+			DEBUG_LOG("WORLD: HandleGossipSelectOptionOpcode - %s not found or you can't interact with it.", guid.GetString().c_str());
+            return;
+		}
+		if (!sScriptDevAIMgr.OnGossipSelect_Item(_player, pItem, sender, action, nullptr))
+		{
+			
+		}
+	}
 }
 
 void WorldSession::HandleSpiritHealerActivateOpcode(WorldPacket& recv_data)
